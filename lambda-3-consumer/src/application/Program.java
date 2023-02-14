0ad6c6b0eea3118fb31bb370d64ec0c7085ab464
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2023-02-14 17:31:35 -0300

    Implementação da interface

---
+++ lambda-3-consumer/src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import entities.Product;
+import util.PriceUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        //Reajusta os preços em 10%
+        list.forEach(new PriceUpdate());
+
+        list.forEach(System.out::println);
+    }
+}
