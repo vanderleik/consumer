a2e4ab008827096ce64e008c27990986340db74d
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2023-02-14 17:37:37 -0300

    Reference method com método não estático

--- lambda-3-consumer/src/application/Program.java
+++ lambda-3-consumer/src/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
         list.add(new Product("HD Case", 80.90));
 
         //Reajusta os preços em 10%
-        list.forEach(Product::staticPriceUpdate);
+        list.forEach(Product::nonStaticPriceUpdate);
 
         list.forEach(System.out::println);
     }
